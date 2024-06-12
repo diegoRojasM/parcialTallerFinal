2423f2904e0ebebb7234ba145332996529ab668d
diegoRojasM <diegohelfo@gmail.com>
2024-06-12 17:30:39 -0400

    segundo commit

--- Examen4/Program.cs
+++ Examen4/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllersWithViews();
 
 
+
 //
 builder.Services.AddDbContext<AppDbContext>(
     options=> options.UseSqlServer(builder.Configuration.GetConnectionString("Defaultt"))
